02233889bbe4d4530f4202ab7d306ac852ec92b7
HoangVietPW26 <nhviet26@gmail.com>
2025-01-24 00:35:13 +0000

    fix deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider
+        afterSignOutUrl="/sign-in"
         appearance={{
           layout: {
             logoImageUrl: '/icons/yoom-logo.svg',
