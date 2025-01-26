c377bd519b16b0549c51b5d0c02ba584a56a83b8
HoangVietPW26 <nhviet26@gmail.com>
2025-01-26 13:51:59 +0000

    fix button border

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
             <Link 
               href={link.route} 
               key={link.label} 
-              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+              className={cn('flex gap-4 items-center p-4 rounded-[6px] justify-start', {
                 'bg-blue-1': isActive
               })}
             >
